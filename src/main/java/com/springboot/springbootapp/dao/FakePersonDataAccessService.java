4f1bd26eb74bdece8a77224a47cbec0b3a54b6c0
mhakash5000 <mhakash5000@gmail.com>
2022-03-03 18:58:30 +0600

    spring-boot rest api

---
+++ src/main/java/com/springboot/springbootapp/dao/FakePersonDataAccessService.java
@@ -0,0 +1,23 @@
+package com.springboot.springbootapp.dao;
+
+import com.springboot.springbootapp.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository("fakeDao")
+public class FakePersonDataAccessService implements PersonDao{
+    private static List<Person> DB = new ArrayList<Person>();
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        DB.add(new Person(id,person.getName()));
+        return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
+    }
+}
---
+++ src/main/java/com/springboot/springbootapp/dao/PersonDao.java
@@ -0,0 +1,15 @@
+package com.springboot.springbootapp.dao;
+
+import com.springboot.springbootapp.model.Person;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PersonDao {
+    int insertPerson(UUID id,Person person);
+    default int insertPerson(Person person){
+        UUID id = UUID.randomUUID();
+        return insertPerson(id,person);
+    }
+    List<Person> selectAllPeople();
+}
